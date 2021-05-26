72568974998e2aea6acceacc69a90ff5c04701b8
IssamLynx <hm_bakha@esi.dz>
2021-05-26 14:46:14 +0100

    update borne

--- app/src/main/java/com/sil1/autolibdz_rental/data/model/Borne.kt
+++ app/src/main/java/com/sil1/autolibdz_rental/data/model/Borne.kt
@@ -2,7 +2,7 @@ package com.sil1.autolibdz_rental.data.model
 
 //import com.google.gson.annotations.SerializedName
 
-data class Borne(
+data class  Borne(
 //    @SerializedName("pk")
     var idBorne:Int,
 //    @SerializedName("nomBorne")
