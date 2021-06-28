9bd9490618e0af37f54f08b77b14c1df31a47363
IssamLynx <hm_bakha@esi.dz>
2021-06-28 18:26:20 +0100

    commit to pull profil

--- app/src/main/java/com/sil1/autolibdz_rental/ui/view/fragment/profil/ProfilFragment.kt
+++ app/src/main/java/com/sil1/autolibdz_rental/ui/view/fragment/profil/ProfilFragment.kt
@@ -1,7 +1,9 @@
 package com.sil1.autolibdz_rental.ui.view.fragment.profil
 
 import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,7 +15,10 @@ import com.sil1.autolibdz_rental.data.model.LocataireRetro
 import kotlinx.android.synthetic.main.fragment_profil.*
 import java.util.ArrayList
 import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
+import com.sil1.autolibdz_rental.data.model.LocataireEditEmail
 import com.sil1.autolibdz_rental.ui.view.activity.MyDrawerController
+import com.sil1.autolibdz_rental.utils.sharedPrefFile
 
 
 //// TODO: Rename parameter arguments, choose names that match
@@ -33,7 +38,6 @@ class ProfilFragment : Fragment() {
 
     var list= mutableListOf<String>()
     var list2= mutableListOf<String>()
-    var boolean:Boolean=false
     private var myDrawerController: MyDrawerController? = null
 
     override fun onAttach(activity: Activity) {
@@ -55,62 +59,44 @@ class ProfilFragment : Fragment() {
     }
     override fun onDestroyView() {
         super.onDestroyView()
-       // myDrawerController?.setDrawer_Locked()
+        // myDrawerController?.setDrawer_Locked()
     }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        val preferences = requireActivity().getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+        val token = preferences.getString("token", "default")
+        val userID = preferences.getString("userID", "1")
+        Log.i("iddd",userID.toString())
+        Log.i("Fragment",token.toString())
+
         var viewModel = ViewModelProvider(this).get(ProfilViewModel::class.java)
-       viewModel.getLocataire()
+        viewModel.getLocataire(token,userID)
         viewModel.locataire.observe(requireActivity(), Observer {
                 locataire ->
             updateLocataire(locataire)
         })
-        reseverButtonD.setOnClickListener{
-            if(!(list2.isEmpty()))
-            if(!list.equals(list2))
-            {
-               viewModel.updateLocataire(locataire,"193") //il faut changer l'id après!
-            //    Log.i("testTAG", "Display locataire List: call enqueue")
-                if(boolean)
-                Toast.makeText(requireActivity(), "mise à jour avec succès", Toast.LENGTH_LONG).show()
-                else
-                Toast.makeText(requireActivity(), "erreur de mise à jour", Toast.LENGTH_LONG).show()
-            }
-
-            nom.text.clear()
-            prenom.text.clear()
-            email.text.clear()
+        SauvgarderP.setOnClickListener{
+            val locataire=LocataireEditEmail(email.text.toString(),password.text.toString())
+            viewModel.updateMailLocataire(locataire,token,userID,requireActivity()) //il faut changer l'id après!
             password.text.clear()
+            viewModel.getLocataire(token,userID)
 
         }
 
+        changerMdp.setOnClickListener{
+            view?.findNavController()?.navigate(R.id.action_nav_profil_to_motDePasseFragment)
+
+        }
     }
     private fun updateLocataire(locataire: ArrayList<LocataireRetro?>)
     {
 
-            nom.setText(locataire.get(0)?.nom)
-            prenom.setText(locataire.get(0)?.prenom)
-            email.setText(locataire.get(0)?.email)
+        nom.setText(locataire.get(0)?.nom)
+        prenom.setText(locataire.get(0)?.prenom)
+        email.setText(locataire.get(0)?.email)
+        list.add(locataire.get(0)?.email.toString())
 
     }
 
-//    companion object {
-//        /**
-//         * Use this factory method to create a new instance of
-//         * this fragment using the provided parameters.
-//         *
-//         * @param param1 Parameter 1.
-//         * @param param2 Parameter 2.
-//         * @return A new instance of fragment ProfilFragment.
-//         */
-//        // TODO: Rename and change types and number of parameters
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            ProfilFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-//            }
-//    }
+
 }
