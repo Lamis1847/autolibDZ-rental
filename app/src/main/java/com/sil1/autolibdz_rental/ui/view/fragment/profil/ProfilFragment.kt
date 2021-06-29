09d0fdf976d517f3bd9d0c1d8995a7e83631353d
IssamLynx <hm_bakha@esi.dz>
2021-06-29 04:35:36 +0100

    fixing conflicts

--- app/src/main/java/com/sil1/autolibdz_rental/ui/view/fragment/profil/ProfilFragment.kt
+++ app/src/main/java/com/sil1/autolibdz_rental/ui/view/fragment/profil/ProfilFragment.kt
@@ -21,20 +21,10 @@ import com.sil1.autolibdz_rental.ui.view.activity.MyDrawerController
 import com.sil1.autolibdz_rental.utils.sharedPrefFile
 
 
-//// TODO: Rename parameter arguments, choose names that match
-//// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//private const val ARG_PARAM1 = "param1"
-//private const val ARG_PARAM2 = "param2"
-//
-///**
-// * A simple [Fragment] subclass.
-// * Use the [ProfilFragment.newInstance] factory method to
-// * create an instance of this fragment.
-// */
+
 class ProfilFragment : Fragment() {
 //    // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
+
 
     private var myDrawerController: MyDrawerController? = null
 
