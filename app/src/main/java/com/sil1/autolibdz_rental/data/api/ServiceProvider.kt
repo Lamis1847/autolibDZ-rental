bdc3f42b3623c42a570d0b576136a73e2221e583
romaissalmh <hr_kessi@esi.dz>
2021-07-09 00:05:18 +0100

    deleted serialized

--- app/src/main/java/com/sil1/autolibdz_rental/data/api/ServiceProvider.kt
+++ app/src/main/java/com/sil1/autolibdz_rental/data/api/ServiceProvider.kt
@@ -1,6 +1,5 @@
 package com.sil1.autolibdz_rental.data.api
 
-import com.google.gson.annotations.SerializedName
 import com.sil1.autolibdz_rental.data.model.*
 import okhttp3.ResponseBody
 import retrofit2.Call
