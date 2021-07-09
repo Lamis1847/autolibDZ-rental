d304172c39c93902ea6f3d18663dec1c30c988f6
IssamLynx <hm_bakha@esi.dz>
2021-07-09 20:34:56 +0100

    modification in design of profil

--- app/src/main/java/com/sil1/autolibdz_rental/data/repositories/ReservationRepository.kt
+++ app/src/main/java/com/sil1/autolibdz_rental/data/repositories/ReservationRepository.kt
@@ -81,12 +81,12 @@ class ReservationRepository {
             }
         }
 
-        private fun checkNetwork(): Boolean {
-            val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
-            val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
-            return isConnected
-        }
+            private fun checkNetwork(): Boolean {
+                val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+                val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+                val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
+                return isConnected
+            }
 
         fun  ajouterReservation(
             TAG: String,
